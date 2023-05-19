5348e0ee61e82394dae881e3c379b794bc713566
JankiParikh <133919208+JankiParikh@users.noreply.github.com>
2023-05-19 17:35:25 -0400

    Add files via upload

---
+++ 19TH MAY EXCERCISE 1 ANSWERS.sql
@@ -0,0 +1,8 @@
+use abcd12;
+SELECT * from daysorder
+where ORD_AMOUNT >= 3*1500 ; 
+select TITLE, CLASS 
+FROM student
+WHERE CLASS = 'V' ;
+SELECT * FROM daysorder
+where ORD_AMOUNT >=3* ADVANCE_AMOUNT;
